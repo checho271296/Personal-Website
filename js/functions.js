d4ed07dd8da4b2d6efa281eb844e3474451a48be
Sergio Fonseca <sergio.fonseca2712@hotmail.com>
2020-07-12 15:28:41 -0600

    improve images font and colors

--- js/functions.js
+++ js/functions.js
@@ -1,8 +1,27 @@
 function myFunction() {
-    var x = document.getElementById("myTopnav");
+  var x = document.getElementById("myTopnav");
     if (x.className === "navbar") {
       x.className += " responsive";
     } else {
       x.className = "navbar";
     }
+    
   }
+
+  const [red, green, blue] = [69, 111, 225]
+  const section1 = document.querySelector('.navbar')
+  var x = document.getElementById("myTopnav");
+  window.addEventListener('scroll', () => {
+    if (window.pageYOffset === 0){
+      section1.style.backgroundColor = `transparent`
+    }
+    else{
+      let y = 1 + (window.scrollY || window.pageYOffset) / 150
+      y = y < 1 ? 1 : y // ensure y is always >= 1 (due to Safari's elastic scroll)
+      const [r, g, b] = [red/y, green/y, blue/y].map(Math.round)
+      section1.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
+    }
+    
+  })
+
+ 
