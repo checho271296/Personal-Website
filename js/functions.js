e2dfc18d82963fcadd3fae3a70bcafc2ab411394
Sergio Fonseca <sergio.fonseca2712@hotmail.com>
2020-07-12 16:25:44 -0600

    fix mergin

--- js/functions.js
+++ js/functions.js
@@ -10,16 +10,18 @@ function myFunction() {
 
   const [red, green, blue] = [69, 111, 225]
   const section1 = document.querySelector('.navbar')
-  var x = document.getElementById("myTopnav");
+  
   window.addEventListener('scroll', () => {
     if (window.pageYOffset === 0){
-      section1.style.backgroundColor = `transparent`
+      section1.style.backgroundColor = `rgb(102, 101, 101)`
+      
     }
     else{
       let y = 1 + (window.scrollY || window.pageYOffset) / 150
       y = y < 1 ? 1 : y // ensure y is always >= 1 (due to Safari's elastic scroll)
       const [r, g, b] = [red/y, green/y, blue/y].map(Math.round)
       section1.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
+      
     }
     
   })
